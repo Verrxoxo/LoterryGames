f24a718f53f83465b54aa700f08e61a589c72497
Verrxoxo <minhanh.kossakowska@gmail.com>
2024-07-25 11:32:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace LoterryGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLottery = new System.Windows.Forms.Button();
+            this.txtLotto1 = new System.Windows.Forms.TextBox();
+            this.txtLotto2 = new System.Windows.Forms.TextBox();
+            this.txtLotto3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLottery
+            // 
+            this.btnLottery.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLottery.Location = new System.Drawing.Point(112, 350);
+            this.btnLottery.Name = "btnLottery";
+            this.btnLottery.Size = new System.Drawing.Size(155, 61);
+            this.btnLottery.TabIndex = 0;
+            this.btnLottery.Text = "Lotter";
+            this.btnLottery.UseVisualStyleBackColor = true;
+            this.btnLottery.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLotto1
+            // 
+            this.txtLotto1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtLotto1.Location = new System.Drawing.Point(501, 68);
+            this.txtLotto1.Multiline = true;
+            this.txtLotto1.Name = "txtLotto1";
+            this.txtLotto1.Size = new System.Drawing.Size(96, 97);
+            this.txtLotto1.TabIndex = 4;
+            this.txtLotto1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLotto1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtLotto2
+            // 
+            this.txtLotto2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtLotto2.Location = new System.Drawing.Point(619, 68);
+            this.txtLotto2.Multiline = true;
+            this.txtLotto2.Name = "txtLotto2";
+            this.txtLotto2.Size = new System.Drawing.Size(96, 97);
+            this.txtLotto2.TabIndex = 5;
+            this.txtLotto2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtLotto2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtLotto3
+            // 
+            this.txtLotto3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtLotto3.Location = new System.Drawing.Point(747, 68);
+            this.txtLotto3.Multiline = true;
+            this.txtLotto3.Name = "txtLotto3";
+            this.txtLotto3.Size = new System.Drawing.Size(96, 97);
+            this.txtLotto3.TabIndex = 6;
+            this.txtLotto3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(549, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 39);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Lottery Game";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(306, 350);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 61);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(545, 215);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 223);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(628, 266);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(247, 223);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(873, 501);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLotto3);
+            this.Controls.Add(this.txtLotto2);
+            this.Controls.Add(this.txtLotto1);
+            this.Controls.Add(this.btnLottery);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLottery;
+        private System.Windows.Forms.TextBox txtLotto1;
+        private System.Windows.Forms.TextBox txtLotto2;
+        private System.Windows.Forms.TextBox txtLotto3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoterryGames
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int iStore;
+        DialogResult iExit;
+
+        private List<CheckBox> CheckBoxes = new List<CheckBox>();  
+        public Form1()
+        {
+            InitializeComponent();
+            CreateCheckBoxes();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer4.Enabled = true;
+        }
+        private void CreateCheckBoxes()
+        {
+            int startX = 10;
+            int startY = 10;
+            int spacing = 30;
+            int columns = 5;
+
+            for (int i = 0; i < 50; i++)
+            {
+                CheckBox checkBox = new CheckBox();
+                checkBox.Text = "Number " + (i + 1);
+                checkBox.Name = i.ToString();
+                checkBox.AutoSize = true;
+
+                int row = i / columns;
+                int column = i % columns;
+
+                checkBox.Left = startX + (column * spacing * 3);
+                checkBox.Top = startY + (row * spacing);
+
+                this.Controls.Add(checkBox);
+                CheckBoxes.Add(checkBox);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer3.Enabled = true;
+            iStore = 20;
+
+            string result = "";
+            foreach (Control control in this.Controls)
+            {
+                if (control is CheckBox checkBox)
+                {
+                    result += $"{checkBox.Text}: {(checkBox.Checked ? "Checked" : "Not Checked")}\n";
+                }
+            }
+            MessageBox.Show(result, "Checkbox States", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int num1;
+            num1 = rnd.Next(1, 50);
+            txtLotto1.Text = Convert.ToString(num1);
+
+            if (txtLotto1.Text == "0")
+            {
+                int num4;
+                num4 = rnd.Next(1, 50);
+                txtLotto1.Text = Convert.ToString(num4);
+            }
+
+            if (txtLotto1.Text == txtLotto2.Text || txtLotto1.Text == txtLotto3.Text)
+            {
+                num1 = rnd.Next(1, 50);
+                txtLotto1.Text = Convert.ToString(num1);
+            }
+            txtLotto1.BackColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            iExit = MessageBox.Show("Confirm if you want to exit", "Lottery Game" ,MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (iExit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int num2;
+            num2 = rnd.Next(1, 50);
+            txtLotto2.Text = Convert.ToString(num2);
+
+            if (txtLotto2.Text == "0")
+            {
+                int num4;
+                num4 = rnd.Next(1, 50);
+                txtLotto2.Text = Convert.ToString(num4);
+            }
+
+            if (txtLotto1.Text == txtLotto2.Text || txtLotto2.Text == txtLotto3.Text)
+            {
+                num2 = rnd.Next(1, 50);
+                txtLotto1.Text = Convert.ToString(num2);
+            }
+            txtLotto2.BackColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int num3;
+            Random rnd = new Random();
+            num3 = rnd.Next(1, 50);
+            txtLotto3.Text = Convert.ToString(num3);
+            
+            if (txtLotto3.Text == "0")
+            {
+                num3 = rnd.Next(1, 50);
+                txtLotto3.Text = Convert.ToString(num3);
+            }
+            
+            if (txtLotto3.Text == txtLotto1.Text || txtLotto3.Text == txtLotto2.Text)
+            {
+                int num4;
+                num4 = rnd.Next(1, 50);
+                txtLotto3.Text = Convert.ToString(num4);
+            }
+
+            txtLotto3.BackColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            label1.ForeColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            btnLottery.BackColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            btnLottery.ForeColor = Color.FromArgb(255, rnd.Next(255), rnd.Next(255), rnd.Next(255));
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            iStore = iStore = 0;
+
+            if (iStore == 0)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                timer3.Enabled = false;
+                timer4.Enabled = false;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
